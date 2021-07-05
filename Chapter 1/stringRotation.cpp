1f92f6763db5fc236d7e523e07bcd1dfb449d398
Alexis Osnaya <alexis.osnaya.ipn@gmail.com>
2021-07-04 20:13:56 -0500

    Problem 1.9 - String rotation

---
+++ Chapter 1/stringRotation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isRotation(string str, string str2){
+    if(str.length() == str2.length() && str.length() > 0){
+         //return isSubstring(str+str,str2)
+    }
+       
+    return false;
+}
+
+int main(){
+    return 0;
+}
